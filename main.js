64b39c2a5b2c3a0a074bd5e2f1748efd45cff44d
armstrongluiza <114159546+armstrongluiza@users.noreply.github.com>
2024-05-05 15:56:48 -0300

    Update main.js

--- main.js
+++ main.js
@@ -85,6 +85,10 @@ function classificaSenha(tamanhoAlfabeto){
     } else if (entropia <= 35){
         forcaSenha.classList.add('fraca');
     }
+const valorEntropia = document.querySelector('.entropia');
+valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
+
+
 }
 
 
